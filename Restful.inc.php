86b8c1bec9789fe080dab73d17eadc10dba01bc4
williampudell <williampudell@gmail.com>
2017-02-04 18:02:16 -0200

    Atualização
    criada a função inputs()

--- Restful.inc.php
+++ Restful.inc.php
@@ -12,5 +12,24 @@
 		public function __construct(){
 			$this->inputs();
 		}
+
+		private function inputs(){
+			switch($this->get_request_method()){
+				case "POST":
+					$this->_request = $this->cleanInputs($_POST);
+					break;
+				case "GET":
+				case "DELETE":
+					$this->_request = $this->cleanInputs($_GET);
+					break;
+				case "PUT":
+					parse_str(file_get_contents("php://input"),$this->_request);
+					$this->_request = $this->cleanInputs($this->_request);
+					break;
+				default:
+					$this->response('',406);
+					break;
+			}
+		}
 	}
 ?>
